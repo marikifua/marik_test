5a0822fc49bfc3f860de987436db7bfb0c1560d0
marikifua <marik_if_ua@ukr.net>
2019-02-14 15:52:16 +0000

    Added 2 commit

--- myscript.sh
+++ myscript.sh
@@ -4,3 +4,4 @@ tar -cvzf ~/`date +"%k-%M"`files.tar.gz ~/dir4
 find /home/ec2-user -mmin +30 -name "*.tar.gz" -exec aws s3 cp "{}" s3://devopstest11127/ \;
 find ~/ -mmin +30 -name "*.tar.gz" -exec rm -f {} \;
 # added change to script 1
+# added 2 co
