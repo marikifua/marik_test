bedfbc085c15c393d012b7c7b8f822ad14b6a314
marikifua <marik_if_ua@ukr.net>
2019-02-14 10:15:43 +0000

    Initial commit

---
+++ myscript.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+tar -cvzf ~/`date +"%k-%M"`files.tar.gz ~/dir4
+#find -mmin +30 -name "*.tar.gz" -exec aws s3 cp "/home/ec2-user/*.tar.gz" s3://devopstest11127/ \;
+find /home/ec2-user -mmin +30 -name "*.tar.gz" -exec aws s3 cp "{}" s3://devopstest11127/ \;
+find ~/ -mmin +30 -name "*.tar.gz" -exec rm -f {} \;
+
