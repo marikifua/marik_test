98b73cb5de9161fa2512d5b3ee4d8ddaa020a3b0
marikifua <marik_if_ua@ukr.net>
2019-02-14 18:28:54 +0200

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -2,7 +2,7 @@
 tar -cvzf ~/`date +"%k-%M"`files.tar.gz ~/dir4
 #find -mmin +30 -name "*.tar.gz" -exec aws s3 cp "/home/ec2-user/*.tar.gz" s3://devopstest11127/ \;
 find /home/ec2-user -mmin +30 -name "*.tar.gz" -exec aws s3 cp "{}" s3://devopstest11127/ \;
-find ~/ -mmin +50 -name "*.tar.gz" -exec rm -f {} \;
+find ~/ -mmin +70 -name "*.tar.gz" -exec rm -f {} \;
 # added change to script 1
 # added 2 co
 
