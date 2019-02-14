6ae9b999b40b71bb424a387b0f61fa879444bb98
marikifua <marik_if_ua@ukr.net>
2019-02-14 14:23:31 +0000

    Added changes to script

--- myscript.sh
+++ myscript.sh
@@ -3,4 +3,4 @@ tar -cvzf ~/`date +"%k-%M"`files.tar.gz ~/dir4
 #find -mmin +30 -name "*.tar.gz" -exec aws s3 cp "/home/ec2-user/*.tar.gz" s3://devopstest11127/ \;
 find /home/ec2-user -mmin +30 -name "*.tar.gz" -exec aws s3 cp "{}" s3://devopstest11127/ \;
 find ~/ -mmin +30 -name "*.tar.gz" -exec rm -f {} \;
-
+# added change to script 11
